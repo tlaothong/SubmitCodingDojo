8d63e24436918ef4e5a5d644c354b98346b95e1b
Teerachai Laothong <tlaothong@gmail.com>
2018-11-30 02:42:20 +0700

    Done thru billion

--- Homework11/Hw11.Lib.Tests/Homework11Tests.cs
+++ Homework11/Hw11.Lib.Tests/Homework11Tests.cs
@@ -8,10 +8,14 @@ namespace Hw11.Lib.Tests
         [Theory]
         [InlineData(0, "ศูนย์")]
         [InlineData(1, "หนึ่ง")]
-        // [InlineData(10, "สิบ")]
-        // [InlineData(20, "ยี่สิบ")]
-        // [InlineData(121, "หนึ่งร้อยยี่สิบเอ็ด")]
-        // [InlineData(5034, "ห้าพันสามสิบสี่")]
+        [InlineData(10, "สิบ")]
+        [InlineData(20, "ยี่สิบ")]
+        [InlineData(121, "หนึ่งร้อยยี่สิบเอ็ด")]
+        [InlineData(5034, "ห้าพันสามสิบสี่")]
+        [InlineData(876530, "แปดแสนเจ็ดหมื่นหกพันห้าร้อยสามสิบ")]
+        [InlineData(1234567, "หนึ่งล้านสองแสนสามหมื่นสี่พันห้าร้อยหกสิบเจ็ด")]
+        [InlineData(120_000_050, "หนึ่งร้อยยี่สิบล้านห้าสิบ")]
+        [InlineData(1200_000_000, "หนึ่งพันสองร้อยล้าน")]
         public void GetReadWordOfNumberTests(int number, string expected)
         {
             var sut = new Homework11();
---
+++ Homework11/Hw11.Lib/SignificantDigitText.cs
@@ -0,0 +1,6 @@
+public class SignificantDigitText
+{
+    public int Value { get; set; }
+    public string Text { get; set; }
+    public bool AlwaysShow { get; set; }
+}
